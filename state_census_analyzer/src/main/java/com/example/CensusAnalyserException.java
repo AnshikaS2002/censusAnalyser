c673e524bba5c2e8cd94ec0713bbea925a8dc494
Anshika Semwal <semwal.anshi.10@gmail.com>
2023-12-31 13:16:57 +0530

    loaded census data with iterator

---
+++ state_census_analyzer/src/main/java/com/example/CensusAnalyserException.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class CensusAnalyserException extends Exception {
+    public enum CensusExceptionType {
+        SOME_IO_EXCEPTION, NO_SUCH_CLASS
+    }
+
+    public CensusExceptionType type;
+    private String message;
+
+    public CensusAnalyserException() {
+    }
+
+    public CensusAnalyserException(CensusExceptionType type, String message) {
+        super(message);
+        this.type = type;
+    }
+}
---
+++ state_census_analyzer/src/main/java/com/example/StateCensusAnalyser.java
@@ -0,0 +1,49 @@
+package com.example;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
+public class StateCensusAnalyser {
+
+    public StateCensusAnalyser() {
+    }
+
+    public static <T> int openCsvBuilder(String csvFilePath, Object myClass) throws CensusAnalyserException {
+        int counter = 0;
+        try {
+            Iterator<Object> myIterator = getIterator(csvFilePath, myClass);
+            while (myIterator.hasNext()) {
+                counter++;
+            }
+        } catch (CensusAnalyserException e) {
+            throw e;
+        }
+        return counter;
+    }
+
+    public static Iterator<Object> getIterator(String csvFilePath, Object myClass) throws CensusAnalyserException {
+        Reader reader = null;
+        CsvToBean<Object> csvToBean;
+        try {
+            reader = Files.newBufferedReader(Paths.get(csvFilePath));
+            csvToBean = new CsvToBeanBuilder(reader)
+                    .withType((Class) myClass)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+
+            Iterator<Object> myIterator = csvToBean.iterator();
+            return myIterator;
+        } catch (IOException e) {
+            throw new CensusAnalyserException(CensusAnalyserException.CensusExceptionType.SOME_IO_EXCEPTION,
+                    "Some IO related exception");
+        }
+    }
+
+}
